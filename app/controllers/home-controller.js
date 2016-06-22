cb4f4cb752fe33f46a467089a24ba9f2f8a5d2d9
avg <atanasgeorgiev83@gmail.com>
2016-06-22 17:11:48 +0300

    Initial revision

---
+++ app/controllers/home-controller.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    function HomeController() {
+        var vm = this;
+
+        //cats.searchCats({ page: 1 })
+        //    .then(function (initialCats) {
+        //        vm.cats = initialCats;
+        //    });
+    }
+
+    angular.module('myServerApp.controllers')
+        .controller('HomeController', [HomeController]);
+}());
